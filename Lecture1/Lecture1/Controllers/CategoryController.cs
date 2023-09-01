6c1389ae0cb249ae1aa6faedcb79fd411a1de7f1
umer-farooq-dev <ufkeyfans786@gmail.com>
2023-09-01 15:47:52 +0500

    Learning

---
+++ Lecture1/Lecture1/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using Lecture1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Lecture1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public static List<Category> listofCategories = new List<Category>
+        {
+            new Category{ Id= 1, Title="Samsung", DisplayOrder= 1},
+            new Category{ Id= 2, Title="Huawie", DisplayOrder= 2},
+            new Category{ Id= 3, Title="Vivo", DisplayOrder= 3},
+            new Category{ Id= 4, Title="Techno", DisplayOrder= 4},
+            new Category{ Id= 5, Title="Apple", DisplayOrder= 5},
+        };
+        [HttpGet]
+
+        public IEnumerable<Category> Get()
+        {
+            return listofCategories;
+        }
+        [HttpPost]
+
+        public void Post([FromBody] Category category)
+        {
+            listofCategories.Add(category);
+        }
+
+        [HttpPut("{id}")]
+
+        public void Put(int id, [FromBody] Category category)
+        {
+            listofCategories[id] = category;
+        }
+
+        [HttpDelete("{id}")]
+
+        public void Delete(int id)
+        {
+            listofCategories.RemoveAt(id);
+        }
+    }
+}
---
+++ Lecture1/Lecture1/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Lecture1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
