fb56fe064ce7bbe302099b20a020b6086313d177
umer-farooq-dev <ufkeyfans786@gmail.com>
2023-09-01 23:43:51 +0500

    Crud Data

--- Lecture1/Lecture1/Data/ApplicatonDbContext.cs
+++ Lecture1/Lecture1/Data/ApplicatonDbContext.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Lecture1.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lecture1.Data
 {
     public class ApplicatonDbContext : DbContext //Collection of Db set
     {
+        public ApplicatonDbContext(DbContextOptions<ApplicatonDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
     }
 }
